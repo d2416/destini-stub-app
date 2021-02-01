1e84314dc5af43927f5133057300204b0d1f2c46
David <davidpineda.co@gmail.com>
2021-02-01 14:42:13 +0000

    first state of the design

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,71 @@ class DestiniApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Destini App',
-      home: Container(
-        
+      theme: ThemeData.dark(),
+      home: StoyPage(),
+    );
+  }
+}
+
+class StoyPage extends StatefulWidget {
+  @override
+  _StoyPageState createState() => _StoyPageState();
+}
+
+class _StoyPageState extends State<StoyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    'Story text will go here',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: FlatButton(
+                  onPressed: () {},
+                  color: Colors.red,
+                  child: Text(
+                    'Choice 1',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                child: FlatButton(
+                  onPressed: () {},
+                  color: Colors.blue,
+                  child: Text(
+                    'Choice 1',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
